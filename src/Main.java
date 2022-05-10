0a6fe5aee85840ce0d99cc0af79c8ed81abf379d
OlegKarabanov <salem27@mail.ru>
2022-05-10 09:23:15 +0300

    Added LinkedList car example

--- src/Main.java
+++ src/Main.java
@@ -55,5 +55,23 @@ public class Main {
         olegBio.remove(1);
         System.out.println(olegBio);
 
+
+        //Linked List on practice
+        LinkedList<Car> cars = new LinkedList<>();
+        Car ferrari = new Car("Ferrari Spider");
+        Car bugatti = new Car("Veyron");
+        Car mercedes = new Car("Mercedes Benz");
+
+        cars.addAll( Arrays.asList(ferrari,bugatti,mercedes));
+        System.out.println(cars);
+
+        cars.addFirst(new Car("Ford gt300"));
+        cars.addLast(new Car("Fiat"));
+        System.out.println(cars);
+
+        System.out.println(cars.pollFirst());
+        System.out.println(cars);
+
+
     }
 }
